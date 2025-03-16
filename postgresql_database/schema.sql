611545e1a56e37acc912515c497e4099d017bdf7
kanderson <81449406+kandrsn99@users.noreply.github.com>
2025-03-16 18:14:16 -0500

    Update schema.sql

--- postgresql_database/schema.sql
+++ postgresql_database/schema.sql
@@ -1,18 +1,17 @@
 /*
     Create a table if it doesn't exist on start
 */
-CREATE TABLE IF NOT EXISTS TEST
+CREATE TABLE IF NOT EXISTS employees
 (
     id SERIAL PRIMARY KEY,
-    names VARCHAR (255)
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    vehicle VARCHAR(50) NOT NULL
 );
 /*
     Insert some values into that database table
 */
-INSERT INTO TEST (names)
- VALUES
-        ('James'),
-        ('Killian'),
-        ('Gautier'),
-        ('Luman'),
-        ('Maya');
+COPY employees(first_name, last_name, vehicle) 
+    FROM './vehiclestable.csv'
+    DELIMITER ','
+    CSV HEADER;
